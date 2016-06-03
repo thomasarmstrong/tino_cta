added outlier/over-/underflow functionality to histogram class;replaced histogram-interpolation algorithm by built-in numpy; fixed TelDict mask; outsourced shower maximum estimator to class in ctapipe.reco

sandbox/FitGammaLikelihood.py
@@ -4,6 +4,7 @@ from ctapipe.io import CameraGeometry
 from ctapipe.io.camera import _guess_camera_type
 from ctapipe.io.containers import MCShowerData as MCShower
 from ctapipe.utils.linalg import *
+from ctapipe.reco.shower_max import *
 from math import pi, log, sin
 
 from Histogram import nDHistogram
@@ -61,7 +62,7 @@ edges.append( np.concatenate( (
                               )
                             )*u.degree
             )
-from numba import jit
+#from numba import jit
 class FitGammaLikelihood:
     def __init__(self, edges=edges, labels=labels):
         self.seed       = None
@@ -73,7 +74,6 @@ class FitGammaLikelihood:
         self.norm = nDHistogram( edges, labels )
         self.pdf  = None
         
-
     def set_instrument_description(self, telescopes, cameras, optics):
         self.Ver = 'Feb2016'
         self.TelVer = 'TelescopeTable_Version{}'.format(self.Ver)
@@ -85,52 +85,45 @@ class FitGammaLikelihood:
         self.optics     = lambda tel_id : optics [self.OptVer+str(tel_id)]
     
     def set_atmosphere(self, filename):
-        altitude  = []
-        thickness = []
-        atm_file = open(filename, "r")
-        for line in atm_file:
-            if line.startswith("#"): continue
-            altitude .append(float(line.split()[0]))
-            thickness.append(float(line.split()[2]))
+        self.shower_max_estimator = ShowerMaxEstimator(filename)
+        #altitude  = []
+        #thickness = []
+        #atm_file = open(filename, "r")
+        #for line in atm_file:
+            #if line.startswith("#"): continue
+            #altitude .append(float(line.split()[0]))
+            #thickness.append(float(line.split()[2]))
         
-        self.atmosphere = nDHistogram( [np.array(altitude)*u.km], ["altitude"] )
-        self.atmosphere.data = (thickness[0:1]+thickness)*u.g * u.cm**-2
+        #self.atmosphere = nDHistogram( [np.array(altitude)*u.km], ["altitude"] )
+        #self.atmosphere.data = (thickness[0:1]+thickness)*u.g * u.cm**-2
 
         
-    def find_shower_max_height(self,energy,h_first_int,gamma_alt):
-        # offset of the shower-maximum in radiation lengths
-        c = 0.97 * log(energy / (83 * u.MeV)) - 1.32
-        # radiation length in dry air at 1 atm = 36,62 g / cm**2 [PDG]
-        c *= 36.62*u.g * u.cm**-2
-        # showers with a more horizontal direction spend more path length in each atm. layer
-        # the "effective transverse thickness" they have to pass is reduced
-        c *= sin(gamma_alt)
+    #def find_shower_max_height(self,energy,h_first_int,gamma_alt):
+        ## offset of the shower-maximum in radiation lengths
+        #c = 0.97 * log(energy / (83 * u.MeV)) - 1.32
+        ## radiation length in dry air at 1 atm = 36,62 g / cm**2 [PDG]
+        #c *= 36.62 * u.g * u.cm**-2
+        ## showers with a more horizontal direction spend more path length in each atm. layer
+        ## the "effective transverse thickness" they have to pass is reduced
+        #c *= sin(gamma_alt)
         
-        # find the thickness at the height of the first interaction
-        t_first_int = 0.
-        for ii, height1 in enumerate(self.atmosphere.bin_edges[0]):
-            if h_first_int < height1:
-                height2 = self.atmosphere.bin_edges[0][ii-1]
-                thick1  = self.atmosphere.evaluate([height1])
-                thick2  = self.atmosphere.evaluate([height2])
+        ## find the thickness at the height of the first interaction
+        #t_first_int = self.atmosphere.interpolate_linear([h_first_int])
+
+        ## total thickness at shower maximum = thickness at first interaction + thickness traversed to shower maximum
+        #t_shower_max = t_first_int + c
+        
+        ## now find the height with the wanted thickness
+        #for ii, thick1 in enumerate(self.atmosphere.data):
+            #if t_shower_max > thick1:
+                #height1 = self.atmosphere.bin_edges[0][ii-1]
+                #height2 = self.atmosphere.bin_edges[0][ii-2]
+                #thick2  = self.atmosphere.evaluate([height2])
                 
-                t_first_int = (thick2-thick1) / (height2-height1) * (h_first_int.to(u.km) - height1) + thick1
-                break
-
-        # total thickness at shower maximum = thickness at first interaction + thickness traversed to shower maximum
-        t_shower_max = t_first_int + c
-        
-        # now find the height with the wanted thickness
-        for ii, thick1 in enumerate(self.atmosphere.data):
-            if t_shower_max > thick1:
-                height1 = self.atmosphere.bin_edges[0][ii-1]
-                height2 = self.atmosphere.bin_edges[0][ii-2]
-                thick2  = self.atmosphere.evaluate([height2])
-                
-                return (height2-height1) / (thick2-thick1) * (t_shower_max-thick1) + height1
+                #return (height2-height1) / (thick2-thick1) * (t_shower_max-thick1) + height1
 
 
-    #@jit    
+
     def get_parametrisation(self, shower, tel_data):
         
         Energy = shower.energy
@@ -195,7 +188,7 @@ class FitGammaLikelihood:
                 #print ([Energy, d, delta, shower.alt, rho, gamma], npe, pixel_area)
                 #yield ([Energy, d, delta, shower.alt, rho, gamma], npe, pixel_area)
                 #yield ([angle(tel_dir,shower_max_dir), d, delta, shower.alt, rho, gamma], npe, pixel_area)
-                yield ([ alpha, d, delta, rho, gamma], npe, pixel_area)
+                yield ([ alpha, d, rho, gamma], npe, pixel_area)
 
         
     def fill_pdf( self, event=None, coordinates=None, value=None ):
@@ -207,8 +200,10 @@ class FitGammaLikelihood:
             except AttributeError:
                 pass
             # find the height of the shower maximum and set it in the shower container
-            event.mc.h_shower_max = self.find_shower_max_height(event.mc.energy, event.mc.h_first_int, event.mc.alt)
-            
+            event.mc.h_shower_max = self.shower_max_estimator.find_shower_max_height(event.mc.energy, event.mc.h_first_int, event.mc.alt)
+            print(event.mc.h_shower_max)
+            return 
+        
             data = dict( [ (tel_id, tel.photo_electrons) for tel_id, tel in event.mc.tel.items() ] )
             for (coordinates, value, pixel_area) in self.get_parametrisation( event.mc, data ):
                 self.hits.fill(coordinates, value)

sandbox/Histogram.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-from numba import jit
+from scipy import ndimage
+#from numba import jit
 
 class nDHistogram:
     def __init__(self, bin_edges, labels=[""]):
@@ -15,17 +15,8 @@ class nDHistogram:
         self.labels    = labels
         
         self.data = np.zeros( [ len(x)+1 for x in bin_edges ] )
-        
-        """ prepare some arrays needed for the interpolation """
-        self.bins          = [0]*self.dimension
-        self.bin_centres   = [0]*self.dimension
-        self.bin_centres_t = [0]*2
-        self.permutations = [ ]
-        for i in range(2**self.dimension):
-            seq = [0]*self.dimension
-            for j, t in enumerate(seq):
-                seq[j] = 1 if i & 2**j else 0
-            self.permutations.append( seq )
+
+            
     def __str__(self):
         string = "bin edges:\n"
         for label, edge in zip(self.labels, self.bin_edges):
@@ -43,56 +34,38 @@ class nDHistogram:
             return np.digitize(value, axis)
         
     def find_bins(self, args):
-        bins = ()
+        bins = []
         for ij, (arg, axis) in enumerate( zip(args,self.bin_edges)):
             # np.digitize seems to ignore units... convert arg to unit of axis as workaround
             try:
-                bins += ( np.digitize(arg.to(axis.unit), axis), )
+                bins += [ np.digitize(arg.to(axis.unit), axis) ]
             except AttributeError:
-                bins += ( np.digitize(arg, axis), )
+                bins += [ np.digitize(arg, axis) ]
         return bins
     
     def fill(self, args, value=1):
         self.data[ self.find_bins(args) ] += value
-        return 1
     
     def evaluate(self, args):
         return self.data[ self.find_bins(args) ]
     
-    @jit
+
     def interpolate_linear(self, args, out_of_bounds_value=0.):
-        # TODO safeguard against edge querries (see next comment)
-        for ii, (axis, arg) in enumerate(zip(self.bin_edges,args)):
-            bin_ = self.find_bin(arg, axis)
-            self.bins[ii] = bin_
-            # for now, ignore pixel in under-/overflow bins; return default value
-            if len(axis) > 2:
-                if bin_ >= len(axis)-1 or bin_ <= 1: return out_of_bounds_value
-            
-            bin_centre = (axis[bin_] + axis[bin_-1])/2.
-            self.bin_centres_t[0] = (bin_centre,0)
-
-            if arg > bin_centre:
-                self.bin_centres_t[1] = ((axis[bin_] + axis[bin_+1])/2.,1)
-            else:
-                self.bin_centres_t[1] = ((axis[bin_-2] + axis[bin_-1])/2.,-1)
-            
-            
-            if len(axis) == 2:
-                self.bin_centres_t[1] = ((3*axis[0] - axis[1])/2.,0)
-
-
-            self.bin_centres[ii] = self.bin_centres_t[:]
+        bins_u = np.array(self.find_bins(args))
+        bins_l = bins_u - 1
         
-        result = 0.
-        for seq in self.permutations:
-            result_temp = 1
-            data = self.data
-            for digit, centre, bin_, arg in zip(seq, self.bin_centres,self.bins,args):
-                data = data[bin_+centre[digit][1]]
-                result_temp *= abs((arg-centre[digit^1][0])/(centre[1][0]-centre[0][0]))
-            result += result_temp * data
-        return result.value
+        margs       = np.zeros(len(args))
+        bin_u_edges = np.zeros(len(args))
+        bin_l_edges = np.zeros(len(args))
+        for ij, axis in enumerate(self.bin_edges):
+            unit = args[ij].unit
+            margs[ij]       = args[ ij ].value
+            bin_u_edges[ij] = axis[ bins_u[ij]-1 ].to(unit).value
+            bin_l_edges[ij] = axis[ bins_l[ij]-1 ].to(unit).value
+        coordinates =  (margs-bin_u_edges) / (bin_u_edges-bin_l_edges) * (bins_u - bins_l) + bins_u
+        
+        return ndimage.map_coordinates(self.data, [coordinates],order=3)[0] * self.data.unit
+        
     
     def fill_bin(self, value, args):
         self.data[ args ] += value
@@ -104,6 +77,22 @@ class nDHistogram:
             data = data[arg]
         return data
     
+    def get_outlier(self):
+        data = self.data
+        for i in range(self.dimension):
+            data = data[1:-1].sum(axis=0)
+        return self.data.sum() - data
+    def get_overflow(self):
+        data = self.data
+        for i in range(self.dimension):
+            data = data[:-1].sum(axis=0)
+        return self.data.sum() - data    
+    def get_underflow(self):
+        data = self.data
+        for i in range(self.dimension):
+            data = data[1:].sum(axis=0)
+        return self.data.sum() - data    
+
     
     def write(self, filename):
         np.savez_compressed(filename, data=self.data,