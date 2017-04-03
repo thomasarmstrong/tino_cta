4e13ffb0c39a01320ff6fb9d0963388adc73c13b
Tino Michael <tino.michael@cea.fr>
2017-04-03 17:20:16 +0200

    small tweaks to dirac scripts

--- dirac_pilot.sh
+++ dirac_pilot.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+echo "pwd:"
 pwd
+
+echo "ls -lhrt"
 ls -lhrt
-echo $PATH
-echo $LD_LIBRARY_PATH
-echo $PYTHONPATH
 
 export MINICONDA=/cvmfs/cta.in2p3.fr/software/miniconda
 export PATH=$MINICONDA/bin:$PATH
 source $MINICONDA/bin/activate ctapipe_0.3.4
 
+export PATH=./:$PATH
+export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+
+echo '$PATH'
 echo $PATH
+echo '$LD_LIBRARY_PATH'
 echo $LD_LIBRARY_PATH
+echo '$PYTHONPATH'
 echo $PYTHONPATH
+
+echo 'which python'
 which python
 
 export MPLBACKEND=Agg
--- dirac_submit.py
+++ dirac_submit.py
@@ -16,12 +16,23 @@ file1='/vo.cta.in2p3.fr/user/c/ciro.bigongiari/MiniArray9/Simtel/gamma/run1011.s
 file2='/vo.cta.in2p3.fr/user/c/ciro.bigongiari/MiniArray9/Simtel/proton/run10011.simtel.gz'
 j.setInputData([file1])
 
-pilot_args = 'fit_events_hillas.py -i . -f {} -m 50 --tail --events_dir ./ --store'.format(file1.split('/')[-1])
-pilot_args = 'fit_events_hillas.py -i . -f {} -m 50 --tail'.format(file1.split('/')[-1])
+pilot_args = ' '.join(('fit_events_hillas.py',
+                       '-m 50',
+                       '--tail',
+                       '--events_dir ./',
+                       '--store',
+                       '-i . -f',
+                       file1.split('/')[-1]))
 
 input_sandbox = ['modules', 'helper_functions.py',
+                 # python wrapper for the mr_filter wavelet cleaning
                  '/local/home/tmichael/software/jeremie_cta/'
                  'sap-cta-data-pipeline/datapipe/',
+                 # executable for the wavelet cleaning
+                 '/local/home/tmichael/software/ISAP/ISAP/cxx/sparse2d/bin/mr_filter',
+                 # fitsIO library
+                 '/local/home/tmichael/software/fitsio/cfitsio/libcfitsio.so.5.3.39',
+                 # sets up the environment + script that is being run
                  'dirac_pilot.sh', pilot_args.split()[0]]
 
 print "running as: ", pilot_args
