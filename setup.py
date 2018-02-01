c6245c17d8c32d58d5625bf2b0ee87ab8fac0473
Tino Michael <tino.michael@cea.fr>
2018-02-01 17:36:12 +0100

    added scripts to setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,15 @@ setup(
     description='A useful module',
     author='Tino Michael',
     author_email='tino.michael@cea.fr',
-    packages=['tino_cta', 'irf_builder'],
-    scripts=['scripts/make_point-source_irfs.py'],
-    install_requires=['numpy', 'scipy', 'scikit-learn', 'astropy', 'pandas', 'yaml',
-                      'matplotlib', 'gammapy']
+    packages=['tino_cta'],
+    scripts=[
+        'scripts/classify_and_reconstruct.py',
+        'scripts/compare_wave_tail_simple.py',
+        'scripts/fit_events_hillas.py',
+        'scripts/train_classifier.py',
+        'scripts/train_energy_regressor.py',
+        'scripts/write_feature_table.py'
+    ],
+    install_requires=['numpy', 'scipy', 'scikit-learn', 'astropy', 'pandas',
+                      'matplotlib']
 )
